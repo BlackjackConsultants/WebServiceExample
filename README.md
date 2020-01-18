00f47e8183f45e51250cf1fca8a67d8f4c7a3452
jorge perez <jorgeperez00@hotmail.com>
2020-01-18 16:57:30 -0500

    have it working

---
+++ README.md
@@ -0,0 +1,4 @@
+# WCF Service Examples
+## SELF HOST WCF TCP/IP SERVICE
+This is a Microsoft WCF Service based tutorial which tackles the 
+**Self Hosting** with **Fluent Configuration** (done programmatically).
---
+++ TcpBinding.Contracts/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Text;
+
+namespace TcpBinding.Contracts {
+    [ServiceContract]
+    public interface IProductService {
+        [OperationContract]
+        string[] GetStrings();
+    }
+}
